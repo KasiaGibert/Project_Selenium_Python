86f721b7b7d0dba00486f6880e299746b159b70f
Kasia Gibert <katarzyna.gibert@gmail.com>
2023-06-20 15:49:09 +0000

    Corrected longin_test

--- login_test.py
+++ login_test.py
@@ -2,7 +2,6 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
-from selenium.webdriver.support.relative_locator import locate_with
 
 # DANE TESTOWE
 email = "kasia.gibert@gmail.com"
@@ -10,61 +9,55 @@ password = "kasiagibert"
 invalid_password = "kas"
 
 class LoginTest(unittest.TestCase):
-    """ Testy logowania"""
+#Logowanie uzytkownika
 
     def setUp(self):
-        """Przygotowanie testu"""
+        """ Przygotowanie testu """
         # 1. Otwarta strona główna
         self.driver = webdriver.Chrome()
         self.driver.get("https://www.taniaksiazka.pl/")
         self.driver.maximize_window()
         self.driver.implicitly_wait(20)
 
-
-
         # 2. Użytkownik niezalogowany
 
-    def testValidPassword(self):
-        """ Prawidłowe hasło """
-        # 1. Kliknij „Zaloguj sie”
+    def testCorrectLogin(self):
+        """ Logowanie z prawidlowymi danymi """
+        # 1. Kliknij przycisk „Zaloguj sie”
         log_in = self.driver.find_element(By.XPATH, '//*[@id="user-box"]/div[1]/p/a/strong')
         log_in.click()
-        # 2. Wpisz email
+        # 2. Wpisz prawidlowy e-mail
         email_input = self.driver.find_element(By.ID, 'loginEmail')
         email_input.send_keys(email)
-        # 2.  Wpisz hasło
+        # 3.  Wpisz haslo
         password_input = self.driver.find_element(By.ID, 'loginPassword')
         password_input.send_keys(password)
-        # 9. Kliknij zaloguj sie
+        # 4. Kliknij przycisk "Zaloguj sie"
         log_in_button = self.driver.find_element(By.XPATH, '//*[@id="LoginPage"]/form[1]/p/button')
         log_in_button.submit()
 
-        # 10. Sprawdzam poprawność adresu URL
+        # Testy sprawdzajace poprawność adresu URL
         URL = self.driver.current_url
         self.assertEqual("https://www.taniaksiazka.pl/Konto", URL,"Incorrect URL")
 
-        sleep(2)
-
     def testInvalidPassword(self):
-        """ Nieprawidłowe hasło """
-        # 1. Kliknij „Zaloguj sie”
+        """ Logowaanie z nieprawidłowym hasłem """
+        # 1. Kliknij przycisk „Zaloguj sie”
         log_in = self.driver.find_element(By.XPATH, '//*[@id="user-box"]/div[1]/p/a/strong')
         log_in.click()
         # 2. Wpisz email
         email_input = self.driver.find_element(By.ID, 'loginEmail')
         email_input.send_keys(email)
-        # 2.  Wpisz bledne hasło
+        # 3.  Wpisz nieprawidlowe hasło
         password_input = self.driver.find_element(By.ID, 'loginPassword')
         password_input.send_keys(invalid_password)
-        # 9. Kliknij zaloguj sie
+        # 4. Kliknij przycisk "Zaloguj sie"
         log_in_button = self.driver.find_element(By.XPATH, '//*[@id="LoginPage"]/form[1]/p/button')
         log_in_button.submit()
 
-        # 10. Sprawdzam poprawność adresu URL
+        # Testy sprawdzajace poprawność adresu URL
         URL = self.driver.current_url
         self.assertEqual("https://www.taniaksiazka.pl/Konto", URL, "Incorrect URL")
-
-        sleep(2)
 
     def tearDown(self):
         self.driver.quit()
