72a8c5a8ce1ee7426974bc11dfc2d6ad28e2dfba
Kasia Gibert <katarzyna.gibert@gmail.com>
2023-06-21 16:18:31 +0000

    Corrected search_test

--- search_test.py
+++ search_test.py
@@ -6,9 +6,8 @@ from time import sleep
 # DANE TESTOWE
 book = "sapiens"
 
-
 class SearchTest(unittest.TestCase):
-    """ Testy wyszukiwania"""
+# Sprawdzenie funkcjonalności wyszukiwarki
 
     def setUp(self):
         """Przygotowanie testu"""
@@ -18,42 +17,39 @@ class SearchTest(unittest.TestCase):
         self.driver.maximize_window()
         self.driver.implicitly_wait(20)
 
-
-    def testValidPassword(self):
-        """ Prawidłowe wyszukiwanie ksiazki """
-        # 1. Kliknij pole wyszukiwania i wyczysc je, jezeli nie jestr puste
+    def testSearchFunction(self):
+        """ Wyszukiwanie ksiazek zawierajacych okreslona fraze"""
+        # 1. Kliknij w pole wyszukiwania
         search_field = self.driver.find_element(By.ID, 'inputSearch')
-        # 2. Sprawdzani, czy pole wyszukiwarki jest puste
+        # 2. Sprawdz, czy pole wyszukiwania jest puste. Jezeli nie wyczysc je
         if search_field == "":
             print("Pole jest puste")
         else:
             print("Pole nie jest puste, wyczyszcze je")
         search_field.clear()
-
-        # 3. Wpisz nazwe ksiazki
-        search_field.send_keys('sapiens')
-        sleep(3)
+        # 3. Wpisz w pole wyszukiwania fraze "sapiens"
+        search_field.send_keys(book)
         # 4.  Kliknij przycisk wyszukiwania
         search_button = self.driver.find_element(By.XPATH, '//*[@id="search-form"]/fieldset/button')
         search_button.submit()
-        # 5. Sprawdzam poprawność adresu URL
+        # Sprawdzam poprawność adresu URL
         URL = self.driver.current_url
         self.assertEqual("https://www.taniaksiazka.pl/Szukaj/q-sapiens", URL,"Incorrect URL")
-        # 6. Pobranie nazw ksiazek z wynikow wyszukiwania
+        # Pobranie nazw ksiazek z wynikow wyszukiwania
         #books = self.driver.find_elements(By.XPATH, "//div[contains(@class, 'product-main-top-info')]//h2/a")
         books = self.driver.find_elements(By.XPATH, "//a[contains(@class, 'ecommerce-datalayer product-title ')]")
         books_name = [book.get_attribute("textContent") for book in books]
         for name in books_name:
             print("Book name:" + name)
-        # 7. Pobieranie cen ksiazek z wynikow wyszukiwania
+        # Pobieranie cen ksiazek z wynikow wyszukiwania
         prices = self.driver.find_elements(By.XPATH, "//span[contains(@class, 'product-price')]")
         price_values = [price.get_attribute("textContent") for price in prices]
         for price in price_values:
             print("Price:" + price)
-        # 8. Sprawdzanie pierwszych 3 cen hoteli
+        # Sprawdzanie pierwszych 3 cen hoteli
         assert price_values[0] == "28,07 zł"
         assert price_values[1] == "38,93 zł"
-        assert price_values[2] == "29,21 zł"
+        assert price_values[2] == "27,95 zł"
 
     def tearDown(self):
         self.driver.quit()
